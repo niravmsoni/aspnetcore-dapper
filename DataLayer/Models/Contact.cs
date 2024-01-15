4d0b1db2d26dd38692dcaf4dc859e9c51dd2d383
Nirav Soni <soninirav910@gmail.com>
2024-01-15 21:09:33 +0530

    Revert code to match model property with DB table

--- DataLayer/Models/Contact.cs
+++ DataLayer/Models/Contact.cs
@@ -3,10 +3,7 @@
     public class Contact
     {
         public int Id { get; set; }
-
-        //Changing FirstName to FName to test mapping when output from query does not match object. In DB, it's FirstName
-        //public string FirstName { get; set; }
-        public string FName { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Company { get; set; }
